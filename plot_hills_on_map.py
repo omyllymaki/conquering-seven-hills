36bbc407c3af82f9b5642fa72faae453740662c6
Ossi Myllymäki <omyllymaki@gmail.com>
2021-03-16 14:59:21 +0200

    Add hill names to map

--- plot_hills_on_map.py
+++ plot_hills_on_map.py
@@ -1,4 +1,5 @@
 import folium
+from folium import DivIcon
 from folium.plugins import MeasureControl
 
 from constants import HILLS, START_HILL
@@ -7,5 +8,13 @@ m = folium.Map(HILLS[START_HILL], zoom_start=14, control_scale=True)
 
 for name, coordinates in HILLS.items():
     folium.Marker(coordinates, tooltip=name).add_to(m)
+    folium.map.Marker(
+        coordinates,
+        icon=DivIcon(
+            icon_size=(250, 36),
+            icon_anchor=(0, 0),
+            html='<div style="font-size: 15pt">' + name + '</div>',
+        )
+    ).add_to(m)
 m.add_child(MeasureControl())
 m.save("results/hills_on_map.html")
